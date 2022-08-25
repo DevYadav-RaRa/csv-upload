48e27c87a825acf193f33e33cfb3ed5beff645ac
DevYadav-RaRa <dev.yadav@rara.delivery>
2022-08-26 01:03:05 +0530

    [ADD]: Status for Csv Orders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,113 @@
-import React from "react";
+import React,{ useState } from "react";
 import jsonImage from "./image.png";
 import "./App.css"
 
 const App = () => {
+    var fileName;
+    var status = {};
+
+    const getStatus = () => {
+          const url = "http://localhost:8181"
+          const endPoint = "/api/v1/orders/csv/"
+  
+          var requestOptions = {
+            method: 'GET'
+          };
+  
+          fetch(url+endPoint+fileName, requestOptions)
+            .then(response => {return response.text()})
+            .then(data => {
+              status = JSON.parse(data)
+              console.log("Polling Response",data)
+              // status = data
+              var myDiv = document.getElementById("status");
+              myDiv.innerHTML = "";
+
+              if (Object.keys(status['data']['success']).length !== 0){
+                const a = document.createElement('table');
+                const successHead = document.createElement('caption');
+                successHead.innerHTML = 'Success';
+                a.appendChild(successHead);
+
+                const q = document.createElement('tr');
+                const w = document.createElement('th');
+                w.innerHTML = 'Status Code';
+                const e = document.createElement('th');
+                e.innerHTML = 'Status';
+                const r = document.createElement('th');
+                r.innerHTML = 'Message';
+                q.appendChild(w);
+                q.appendChild(e);
+                q.appendChild(r);
+
+                a.appendChild(q);
+                for (const property in status['data']['success']) {
+                  const w = document.createElement('td');
+                  w.innerHTML = JSON.stringify(property);
+
+                  const e = document.createElement('tr');
+                  const r = document.createElement('td');
+                  const t = document.createElement('td');
+                  const y = document.createElement('td');
+                  var x = status['data']['success'][property];
+                  r.innerHTML = x['Status Code'];
+                  t.innerHTML = x['Status'];
+                  y.innerHTML = x['Message'];
+
+                  e.appendChild(r)
+                  e.appendChild(t)
+                  e.appendChild(y)
+
+                  a.appendChild(w)
+                  a.appendChild(e)
+                }
+                myDiv.appendChild(a)
+              }
+
+              if (Object.keys(status['data']['failed']).length !== 0){
+                const b = document.createElement('table');
+                const failedHead = document.createElement('caption');
+                failedHead.innerHTML = 'Failed';
+                b.appendChild(failedHead);
+
+                const g = document.createElement('tr');
+                const h = document.createElement('th');
+                h.innerHTML = 'Status Code';
+                const j = document.createElement('th');
+                j.innerHTML = 'Status';
+                const k = document.createElement('th');
+                k.innerHTML = 'Message';
+                g.appendChild(h);
+                g.appendChild(j);
+                g.appendChild(k);
+
+                b.appendChild(g);
+                for (const property in status['data']['failed']) {
+                  const w = document.createElement('td');
+                  w.innerHTML = JSON.stringify(property);
+
+                  const e = document.createElement('tr');
+                  const r = document.createElement('td');
+                  const t = document.createElement('td');
+                  const y = document.createElement('td');
+                  var x = status['data']['failed'][property];
+                  
+                  r.innerHTML = x['Status Code'];
+                  t.innerHTML = x['Status'];
+                  y.innerHTML = x['Message'];
+
+                  e.appendChild(r)
+                  e.appendChild(t)
+                  e.appendChild(y)
+
+                  b.appendChild(w)
+                  b.appendChild(e)
+                }
+                myDiv.appendChild(b)
+              }
+            })
+            .catch((error) => {console.log(error)})
+        }
 
     const uploadFile = (e) => {
       e.preventDefault();
@@ -16,27 +121,26 @@ const App = () => {
         const BusinessName = "Dasnic"
         const ServiceId = "DasID"
         const Accountnumber = "DAS121"
-        const Servicetype = "MP"  
-        //const WebhookStr = `[{'purpose':["OR","OP","VO","VC"],"requestMethod":"POST","url":'https://envqm7h8tmior.x.pipedream.net',"headers":[{"key":"Key1","value":"BusinessHeader"},{"key":"Key2","value":"BusinessHeader"}],"payload":"tmpl-1"}]`
+        const Servicetype = "MP"
 
         myHeaders.append("tenanttoken", TenantToken);
         myHeaders.append("accountnumber", Accountnumber);
         myHeaders.append("businessname", BusinessName);
         myHeaders.append("serviceid", ServiceId);
         myHeaders.append("servicetype", Servicetype);
-       // myHeaders.append("webhook", WebhookStr);
+
         var requestOptions = {
             method: 'GET',
             headers: myHeaders
         };
         
-
         fetch('https://rara-ms-oms-brs.dev.rara.delivery/api/v1/orders/presigned', requestOptions)
           .then(response => {return response.text()})
           .then((data) => { const resp = JSON.parse(data)
             console.log("DAS",resp)
-
-            console.log("File",text)
+            fileName = resp.data.file
+            console.log("State",fileName)
+            // console.log("File",text)
             const requestOptions = {
               method: 'PUT',
               headers: {
@@ -52,7 +156,10 @@ const App = () => {
             };
 
             fetch(resp.data.presigned_url, requestOptions)
-              .then(response => console.log(response))
+              .then(response => {
+                response.text()
+                getStatus()
+              })
               .catch((error) => {console.log(error)})
           })
           .catch((error) => {console.log(error)})
@@ -62,9 +169,14 @@ const App = () => {
     return (
         <div>
           <div className="flex-container block">
-            <img src={jsonImage}></img>
-            <h2>Upload CSV file here</h2>
-            <input accept=".csv, .xls, .xlsx, text/csv, application/csv, text/comma-separated-values, application/csv, application/excel, application/vnd.msexcel, text/anytext, application/vnd. ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" type="file" id="file" className="file" onChange={uploadFile}/>
+            <div className="flex-container block outerDiv">
+              <img src={jsonImage}></img>
+              <h3>Upload CSV file here</h3>
+              <input accept=".csv, text/csv, application/csv, text/comma-separated-values, application/csv" type="file" id="file" className="file" onChange={uploadFile}/>
+              <button onClick={getStatus}>Update Status</button>
+            </div>
+            <div id="status" className="flex-container block status">
+            </div>
           </div>
         </div>
     );
